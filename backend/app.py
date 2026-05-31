634f29a9509f0ec42d561a55aadd8a9864d9cdc3
Isaque de Medeiros Pinto <132860343+Isaque-Medeiros@users.noreply.github.com>
2026-05-31 10:06:19 -0300

    up

--- backend/app.py
+++ backend/app.py
@@ -361,7 +361,7 @@ def criar_agendamento():
 
     # URL do WhatsApp
     import urllib.parse
-    whatsapp_url = f"https://wa.me/{WHATSAPP_NUMBER}?text={urllib.parse.quote(mensagem)}"
+    whatsapp_url = f"https://wa.me/5511915040871}?text={urllib.parse.quote(mensagem)}"
 
     return jsonify({
         'success': True,
